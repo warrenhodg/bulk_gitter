b79ddba4f2e573baabbaf880b60be126cadfc10f
Warren Hodgkinson <warren.hodgkinson@gmail.com>
2016-09-28 09:30:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,138 @@
+(function(){
+	"use strict";
+});
+
+var cp = require("child_process");
+
+function getNextFile() {
+	var cmd = "git status -s";
+	console.log(cmd);
+
+	return new Promise(function(resolve, reject) {
+		cp.exec(cmd, [], function (error, stdout, stderr) {
+			if (error) {
+				reject(error);
+			} else {
+				lines = stdout.split("\n");
+				if (lines.length >= 0) {
+					var line = lines[0];
+					var parts = line.split(" ");
+					if (parts.length >= 2) {
+						resolve(parts[1]);
+					} else {
+						resolve(null);
+					}
+				} else {
+				   resolve(null);
+				}
+			}
+		});
+	});
+}
+
+function gitAdd(filename) {
+	var cmd = "git add '" + filename + "'";
+	console.log(cmd);
+
+	return new Promise(function (resolve, reject) {
+		cp.exec(cmd, [], function (error, stdout, stderr) {
+			if (error) {
+				reject (error);
+			} else {
+				resolve(null);
+			}
+		});
+	});
+}
+
+function gitCommit(filename) {
+	var cmd = "git commit -m 'Added " + filename + "'";
+	console.log(cmd);
+
+	return new Promise(function (resolve, reject) {
+		cp.exec(cmd, [], function (error, stdout, stderr) {
+			if (error) {
+				reject (error);
+			} else {
+				resolve(null);
+			}
+		});
+	});
+}
+
+function gitPush() {
+	var cmd = "git push origin master";
+	console.log(cmd);
+
+	return new Promise(function (resolve, reject) {
+		cp.exec(cmd, [], function (error, stdout, stderr) {
+			if (error) {
+				reject (error);
+			} else {
+				resolve(null);
+			}
+		});
+	});
+}
+
+function processFiles (resolve, reject) {
+	if (!resolve) {
+		return new Promise(function (_resolve, _reject) {
+			processFiles (_resolve, _reject);
+		});
+	} else {
+		getNextFile()
+
+		.then(function(filename) {
+			if (filename) {
+				gitAdd(filename)
+
+				.then(function() {
+					gitCommit(filename)
+
+					.then(function() {
+						gitPush()
+
+						.then(function () {
+							processFiles(resolve, reject);
+						})
+
+						.catch(function(err1) {
+							reject(err1);
+						});
+					})
+
+					.catch(function(err2) {
+						reject(err2);
+					});
+				})
+
+				.catch(function(err3) {
+					reject(err3);
+				});
+			} else {
+				resolve();
+			}
+		})
+
+		.catch(function(err4) {
+			reject(err4);
+		});
+	}
+}
+
+function main() {
+	processFiles ()
+
+	.then(function() {
+		console.log("Done");
+		process.exit(0);
+	})
+
+	.catch(function(error) {
+		console.error(error);
+		process.exit(1);
+	});
+}
+
+main();
